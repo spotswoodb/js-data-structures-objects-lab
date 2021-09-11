41e10f3a8ba7a08ddee89df2a6e7d3bbe7822c47
Ben Spotswood <spotswoodb@gmail.com>
2021-09-11 14:18:51 -0500

    commit tree 73ed1458641fef94fd7c1e1143b6cdcdcb9db2a6

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution in this file!
+
+const driver = {greeting: "hello"}
+
+function updateDriverWithKeyAndValue(obj, key, value){
+    // return Object.assign({}, obj, {[key]: value})
+  const newObj = { ...obj };
+
+  newObj[key] = value;
+
+  return newObj;
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(obj, key, value){
+    obj[key] = value
+    return obj
+}
+
+function deleteFromDriverByKey(obj, key){
+  const newObj = Object.assign({}, obj)
+  delete newObj[key]
+  return newObj
+}
+
+function destructivelyDeleteFromDriverByKey(obj, key){
+  delete obj[key]
+  return obj
+}
+
+
+// destructivelyDeleteFromDriverByKey() - this function should work the same as
+// deleteFromDriverByKey() but it should mutate the driver passed in. Be sure to consider
+// whether and how using dot notation vs. bracket notation might affect your solution.
